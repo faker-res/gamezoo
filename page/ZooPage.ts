687afccf34314dd6865da91e19efebbf66692867
xiaoer <xiaoer@example.com>
2019-09-11 14:08:25 +0800

    client批量提交

--- page/ZooPage.ts
+++ page/ZooPage.ts
@@ -89,7 +89,7 @@ module gamezoo.page {
 			TongyongPageDef.ins.alertRecharge(StringU.substitute("老板，您的金币少于{0}哦~\n补充点金币去大杀四方吧~", limit), () => {
 				this._game.uiRoot.general.open(DatingPageDef.PAGE_CHONGZHI);
 			}, () => {
-			}, false, PathGameTongyong.ui_tongyong_general + "btn_cz.png");
+			}, false, TongyongPageDef.TIPS_SKIN_STR['cz']);
 		}
 
 		private initPlayerInfo(): void {
